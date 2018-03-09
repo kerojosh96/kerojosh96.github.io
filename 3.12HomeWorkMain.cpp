1bc91c09b03a66de90acd5b093ec356d834b53a2
kerojosh96 <jaybirdcook@gmail.com>
2018-03-09 10:59:45 -0800

    Create 3.12HomeWorkMain.cpp

---
+++ 3.12HomeWorkMain.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Date.h"
+using namespace std;
+
+int main()
+{
+    Date testy(3,2,34);
+    Date testy2(-23,4,2016);
+    cout<< testy.displayDate()<<endl;
+    cout << testy2.displayDate();
+    cout << " the year has been changed to " << testy.setyear(2017)<<endl;
+    cout << "the new year is "<<testy.getYear()<<"(" <<testy.displayDate();
+
+}
