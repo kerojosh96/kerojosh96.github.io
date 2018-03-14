f94faa61fcfa14ce17d6349d0fee01838356d72e
kerojosh96 <jaybirdcook@gmail.com>
2018-03-14 11:52:36 -0700

    Create 3.13HomeWorkAccount.h

---
+++ 3.13HomeWorkAccount.h
@@ -0,0 +1,64 @@
+// Fig. 3.8: Account.h
+// Account class with name and balance data members, and a
+// constructor and deposit function that each perform validation.
+#include <string>
+#include <iostream>
+using namespace std;
+class Account
+{
+public:
+
+    // Account constructor with two parameters
+    Account(std::string accountName, int initialBalance)
+        : name{accountName}   // assign accountName to data member name
+    {
+
+        // validate that the initialBalance is greater than 0; if not,
+        // data member balance keeps its default initial value of 0
+        if (initialBalance > 0)   // if the initialBalance is valid
+        {
+            balance = initialBalance; // assign it to data member balance
+        }
+    }
+
+    // function that deposits (adds) only a valid amount to the balance
+    void deposit(int depositAmount)
+    {
+        if (depositAmount > 0)   // if the depositAmount is valid
+        {
+            balance = balance + depositAmount; // add it to the balance
+        }
+    }
+
+    // function returns the account balance
+    int getBalance() const
+    {
+        return balance;
+    }
+
+    // function that sets the name
+    void setName(std::string accountName)
+    {
+        name = accountName;
+    }
+
+    // function that returns the name
+    std::string getName() const
+    {
+        return name;
+    }
+
+    void displayAccount(Account AccountToDisplay)
+    {
+        std::string a;
+        std::string b;
+        b = AccountToDisplay.getName();
+        a = AccountToDisplay.getBalance();
+        cout <<" account: " <<AccountToDisplay.getName()<< " balance is $"<<AccountToDisplay.getBalance()<<endl;
+
+
+    }
+private:
+    std::string name; // account name data member
+    int balance{0}; // data member with default initial value
+}; // end class Account
